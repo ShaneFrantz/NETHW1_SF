0af27a24e27af9d25480352a749c5fa1f3ca262a
Shane Frantz <shanesfrantz@gmail.com>
2024-01-31 14:28:18 -0600

    completed assignment 2

--- Program.cs
+++ Program.cs
@@ -1,49 +1,102 @@
 ﻿// Shane Frantz
 // .NET Programming CPSC-23000
-// This program is used to calculate the volumes of cubes and spheres based off of user inputted variables
+// This program is used to calculate how many gallons of paint and primer is needed to paint a house
+// Given certain dimensions of the house and parameters about the paint/primer
 
 using System;
 
-namespace HW1
-{
-    class Program
-    {
-        static void Main()
-        {
+namespace HW2 {
+    class Program {
+        static void Main() {
+            // Amount of square feet a gallon of paint and primer will cover per gallon
+            const int paintPerGallon = 400;
+            const int primerPerGallon = 250;
+
+            // Running total of required area to be painted and primed
+            double paintArea = 0;
+            double primeArea = 0;
+
             // Printing header
-            Console.WriteLine("********************************************************************\n\t\t\tShape Calculator V1.0\n********************************************************************");
+            Console.WriteLine("********************************************************************\n\t\t\tHOUSE PAINTER V1.0\n********************************************************************");
+            
+            // Using try block to handle bad user inputs
+            try {
+            // Inputting number of rooms
+            Console.Write("\nHow many rooms do you plan to paint? ");
+            int numberOfRooms = int.Parse(Console.ReadLine());
+                // Ask for dimensions of each room
+                for (int i = 1; i <= numberOfRooms; i++) {
+                    Console.WriteLine($"For Room #{i} ...");
 
-            // Inputting variables for cube volume
-            Console.WriteLine("\nFirst, let's deal with a cube.");
+                    // Getting length
+                    Console.Write("\tEnter length: ");
+                    double length = double.Parse(Console.ReadLine());
 
-            Console.Write("What is the width? ");
-            double width = Convert.ToDouble(Console.ReadLine());
+                    // Getting width
+                    Console.Write("\tEnter width: ");
+                    double width = double.Parse(Console.ReadLine());
 
-            Console.Write("What is the length? ");
-            double length = Convert.ToDouble(Console.ReadLine());
+                    // Getting height
+                    Console.Write("\tEnter height: ");
+                    double height = double.Parse(Console.ReadLine());
 
-            Console.Write("What is the height? ");
-            double height = Convert.ToDouble(Console.ReadLine());
+                    // Boolean args for painting ceiling and using primer
+                    Console.Write("\tPaint the ceiling? ");
+                    string userInput = Console.ReadLine().Trim().ToLower();
 
-            // Calculating and printing cube volume
-            double cubeVolume = width * length * height;
-            Console.WriteLine($"The cube's volume is {cubeVolume:F3}");
+                    // while loop to validate user input
+                    while (userInput != "y" && userInput != "n" && userInput != "yes" && userInput != "no") {
+                        Console.WriteLine("Please enter \"yes\", \"no\", \"y\", or \"n\"");
+                        userInput = Console.ReadLine().Trim().ToLower();
+                    }
 
-            // Inputting radius for sphere volume
-            Console.WriteLine("\nNow let's deal with a sphere.");
-            Console.Write("What is the radius? ");
-            double radius = Convert.ToDouble(Console.ReadLine());
+                    // Sets paintCeiloing after userInput has been validated
+                    bool paintCeiling = userInput == "yes" || userInput == "y";
 
-            // Calculating and printing sphere volume
-            double sphereVolume = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
-            Console.WriteLine($"The sphere's volume is {sphereVolume:F3}");
+                    Console.Write("\tUse primer? ");
+                    userInput = Console.ReadLine().Trim().ToLower();
+                    // while loop to validate user input
+                    while (userInput != "y" && userInput != "n" && userInput != "yes" && userInput != "no") {
+                        Console.WriteLine("Please enter \"yes\", \"no\", \"y\", or \"n\"");
+                        userInput = Console.ReadLine().Trim().ToLower();
+                    }
 
-            // Adding cube and sphere volumes
-            double totalVolume = cubeVolume + sphereVolume;
-            Console.WriteLine($"\nThe total volume of the cube and sphere is {totalVolume:F3}");
+                    // Sets usePrimer after userInput has been validated
+                    bool usePrimer = userInput == "yes" || userInput == "y";
 
-            Console.WriteLine("\nThank you for using this program.");
+                    // Calculating total area that needs to be painted/primed all four walls of the room
+                    double areaOfWalls = (length + width) * height * 2;
+                    double areaOfCeiling = length * width;
+
+                    // Add area of the room to the running totals of paint and primer
+                    if (paintCeiling) {
+                        paintArea += (areaOfWalls + areaOfCeiling);
+                    } else {
+                        paintArea += areaOfWalls;
+                    }
+
+                    if (usePrimer) {
+                        if (paintCeiling) {
+                            primeArea += (areaOfWalls + areaOfCeiling);
+                        } else {
+                            primeArea += areaOfWalls;
+                        }
+                    }
+                }
+
+                // Calculating number of gallons of paint and primer needed
+                double paintGallons = paintArea / paintPerGallon;
+                double primerGallons = primeArea / primerPerGallon;
+
+                // Printing results
+                Console.WriteLine($"\nTo paint {paintArea:F2} square feet,\n\tyou need {paintGallons:F2} gallons of paint.");
+                Console.WriteLine($"To prime {primeArea:F2} square feet,\n\tyou need {primerGallons:F2} gallons of primer.");
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            } finally {
+                // Prints goodbye message even if user generated an error (to make them feel better)
+                Console.WriteLine("\nThank you for using this program.");
+            }
         }
     }
 }
-
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# NETHW1_SF
+# NETHW2_SF
 
-In this assignment, you will write a C# program that will calculate the volume of a cube and of a sphere. The user will enter the relevant dimensions for each, and your program will calculate and display each shape’s volume. It will then show the total volume at the end of the program.
+For this assignment, you will write a C#
+solution
+called
+HousePaintingLastName (where you will replace
+“LastName” with your lastname)
+.
+The program will enable the user to specify
+the dimensions of the
+rooms of the house they want to paint. For each room, the program will ask the user for the length,
+width, and height of the room, whether or not they want to use primer, and whether or not they want
+to paint the ceiling. At the end,
+the program will show them the total square footage they will be
+painting and the total number of gallons of paint and primer required. You can assume that paint covers
+400 square feet per gallon and primer covers 250 square feet per gallon.
